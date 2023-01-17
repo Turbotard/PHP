d02b0a8d911e43eb2ebd40b549482e78acb7379b
Turbotard <turbotard9@gmail.com>
2023-01-17 15:35:27 +0100

    footer qui marche pas

---
+++ www/assets/footer.inc.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="footer.inc.css">
+    <script src="https://kit.fontawesome.com/81dc5c492f.js" crossorigin="anonymous"></script>
+    <link href="https://www.dafontfree.net/embed/bXVsaS1zZW1pYm9sZCZkYXRhLzE2L20vNzg2NTYvTXVsaS1TZW1pQm9sZC50dGY" rel="stylesheet" type="text/css"/>
+
+</head>
+<body class="body">
+<footer class="footer">
+
+<div class="box11">
+
+    <p class="white yo">Information</p>
+    <p class="gris"><span class="orange">Tel : </span>07 83 49 44 12</p>
+    <p class="gris"><span class="orange">Email : </span>aya.aidouni@edu.esiee-it.fr</p>
+    <p class="gris"><span class="orange">Location : </span>Chez AYA</p>
+    <p><a href="https://www.facebook.com/" target="_blank"> <i  class="fa-brands fa-square-facebook"></i></a>
+        <a href="https://www.twitter.com/" target="_blank"> <i class="fa-brands fa-square-twitter"></i></a>
+        <a href="https://www.google.com/" target="_blank"> <i class="fa-brands fa-google"></i></a>
+        <a href="https://www.pinterest.com/" target="_blank"> <i class="fa-brands fa-square-pinterest"></i></a>
+        <a href="https://www.instagram.com//" target="_blank"> <i class="fa-brands fa-square-instagram"></i></a></p>
+
+        <p class="copy">Copyright © 2023 Tous droits réservés</p>
+</div>
+
+
+
+<section class="box22">
+<h5 class="white">Power Of Memory</h5>
+<div id="bullet">
+    <ul class="bullet">
+        <li><a href="">Jouer !</a></li><br>
+        <li><a href="">Les scores</a></li><br>
+        <li><a href="">Aled</a></li>
+    </ul>
+</div>
+</section>
+</footer>
+    
+</body>
+</html>
