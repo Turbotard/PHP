de0b2f5747577cf792fd2057d6912a1b2dc2025d
AyaAid <aya.lakhel11@gmail.com>
2023-01-17 09:08:57 +0100

    package src www

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'BNParihaut',
+        'port' => 8888,
+        'user' => 'root',
+        'pass' => 'root'
+    ]
+];
