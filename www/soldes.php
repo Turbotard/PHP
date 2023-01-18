46f9343a65680301c1ded97d83eceebdf23ae408
nom2 <113614747+Stanewolfer@users.noreply.github.com>
2023-01-18 15:08:48 +0100

    page soldes

---
+++ www/soldes.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/../src/init.php';
+require_once __DIR__ . '/../src/config.php';
+$page_title = 'Mes soldes';
+require_once __DIR__ . '/../src/templates/partials/html_head.php';
+
+
+?>
+<body>
+<?php require_once __DIR__ . '/../src/templates/partials/headers.inc.php';
+?>
+<div>
+    <h1 class="soldes">MES SOLDES</h1>
+</div>
+</div>
+</div>
+<?php require_once __DIR__ . '/../src/templates/partials/bouton_scroll_haut.php'; ?>
+
+<?php require_once __DIR__ . '/../src/templates/partials/footer.inc.php'; ?>
+</body>
+</html>
