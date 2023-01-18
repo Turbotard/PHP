5ef5e579294267e4b869ad9e168fe39dd888793a
nom2 <113614747+Stanewolfer@users.noreply.github.com>
2023-01-18 15:34:18 +0100

    tableau soldes

--- www/myaccount.php
+++ www/myaccount.php
@@ -18,12 +18,12 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
         <h2>NOM : <?php echo ($_SESSION['user']['nom']);?></h2> 
         <h2>PRENOM : <?php echo ($_SESSION['user']['prenom']);?></h2>
         <h2>NUMERO DE COMPTE : <?php echo ($_SESSION['user']['client_number']);?></h2>
-        <a href="/soldes.php"><button class="bouton">MES SOLDES</button></a>
         <h2>GRADE : <?php if($_SESSION['user']['grade'] == 1){
             echo "Client";
         }else if($_SESSION['user']['grade'] == 1000){
             echo "Admin";
         };?></h2>
+        <div class="solde"><a href="/soldes.php"><button class="bouton">MES SOLDES</button></a></div>
     </div>
     <div class="ligne2">
         <h2>MES TRANSACTION</h2>
