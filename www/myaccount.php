3c2d2b206413e1f1fa17caa0fab8ecfd6917408b
Turbotard <turbotard9@gmail.com>
2023-01-19 13:56:07 +0100

    correct

--- www/myaccount.php
+++ www/myaccount.php
@@ -16,14 +16,6 @@ $_SESSION['bank'] = $donnees; // prend tout les comptes en rapport avec le user
 $var2 = $db-> prepare('SELECT * FROM transactions WHERE id_user = ?');
 $var2->execute([$_SESSION['user']['id']]);
 $donnees2 = $var2->fetch();
-$_SESSION['transactions'] = $donnees2;
-$somme = $_SESSION['transactions']['somme']; 
-$currencie = $_SESSION['transactions']['id_currencie'];
-if (isset($_POST['modif_nom']) && !empty($_POST['nom'])) {
-    $nom = $_POST['nom'];
-    $var = $db->prepare('UPDATE users SET nom = ? WHERE id = ?');
-    $var->execute(array($nom, $_SESSION['user']['id']));
-}
 if ($donnees2 == null){
     $verif = false;
 }
@@ -38,7 +30,7 @@ else{
 
 <div>
     <h1 class="espace_banque">MON ESPACE</h1>
-<div class="table">
+</div class="table">
     <div>
     <div class="ligne 1">
         <h2>NOM : <?php echo ($_SESSION['user']['nom']);?></h2> 
@@ -51,26 +43,11 @@ else{
         };?></h2>
         <div class="solde"><a href="/soldes.php"><button class="bouton">MES SOLDES</button></a></div>
     </div> <br>
-    <div class="table">
     <div class="ligne2">
-            <div class="transactions">
+            <div class="historique">
             <h2>MES TRANSACTION</h2>
             <div class="tableau">
             <?php
-
-            // Get the account ID
-            $account_id = $_SESSION['bank'];
-
-            // Prepare and execute the query to retrieve the transactions
-            $query = $db->prepare('SELECT * FROM transactions WHERE id_account = ?');
-            $query->execute([$account_id]);
-            $transactions = $query->fetchAll();
-
-            $query = $db->prepare('SELECT * FROM bankaccounts');
-$query->execute();
-$bankaccounts = $query->fetchAll();
-
-
             // Check if there are any transactions
             if($verif){
 
@@ -84,7 +61,6 @@ $bankaccounts = $query->fetchAll();
             } else {
             echo "Il n'y a pas d'historique de transaction pour ce compte.";
         }
-
         ?>
                     
             </div>
@@ -96,7 +72,7 @@ $bankaccounts = $query->fetchAll();
                     <label for="nom">NOM : 
                         <input type="text" name="nom" id="nom" class="input_white">
                     </label>
-                    <input type="submit" class="bouton_envoi" name="modif_nom" value="MODIFIER">
+                    <input type="submit" class="bouton_envoi" value="MODIFIER">
                 </form>
             </div><br>
             <div class="prénom">
@@ -104,7 +80,7 @@ $bankaccounts = $query->fetchAll();
                     <label for="prénom">PRENOM : 
                         <input type="text" name="prénom" id="prénom" class="input_white">
                     </label>
-                    <input type="submit" class="bouton_envoi" name="modif_prenom" value="MODIFIER">
+                    <input type="submit" class="bouton_envoi" value="MODIFIER">
                 </form>
             </div><br>
             <div class="email">
@@ -112,7 +88,7 @@ $bankaccounts = $query->fetchAll();
                     <label for="email">EMAIL : 
                         <input type="text" name="email" id="email" class="input_white">
                     </label>
-                    <input type="submit" class="bouton_envoi" name="modif_mail" value="MODIFIER">
+                    <input type="submit" class="bouton_envoi" value="MODIFIER">
                 </form>
             </div><br>
             <div class="mdp">
@@ -120,7 +96,7 @@ $bankaccounts = $query->fetchAll();
                     <label for="mdp">MOT DE PASSE : 
                         <input type="text" name="mdp" id="mdp" class="input_white">
                     </label>
-                    <input type="submit" class="bouton_envoi" name="modif_mdp" value="MODIFIER">
+                    <input type="submit" class="bouton_envoi" value="MODIFIER">
                 </form>
             </div><br>
             <div class="dateNaiss">
@@ -128,7 +104,7 @@ $bankaccounts = $query->fetchAll();
                     <label for="dateNaiss">DATE DE NAISSANCE : 
                         <input type="date" name="dateNaiss" id="dateNaiss" class="input_white">
                     </label>
-                    <input type="submit" class="bouton_envoi" name="modif_date" value="MODIFIER">
+                    <input type="submit" class="bouton_envoi" value="MODIFIER">
                 </form>
             </div><br>
             <div class="tel">
@@ -136,7 +112,7 @@ $bankaccounts = $query->fetchAll();
                     <label for="tel">TELEPHONE : 
                         <input type="text" name="tel" id="tel" class="input_white">
                     </label>
-                    <input type="submit" class="bouton_envoi" name="modif_tel" value="MODIFIER">
+                    <input type="submit" class="bouton_envoi" value="MODIFIER">
                 </form>
             </div>
     </div>
