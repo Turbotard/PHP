9a0a4ba6da49f6adbfb6cd444413c9d82831ac78
Turbotard <turbotard9@gmail.com>
2023-01-19 16:54:42 +0100

    acceptations

--- www/accepte.php
+++ www/accepte.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../src/init.php';
 require_once __DIR__ . '/../src/config.php';
-$page_title = 'Mon espace';
+$page_title = 'Acceptations';
 require_once __DIR__ . '/../src/templates/partials/html_head.php';
 
 
@@ -9,15 +9,6 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
 <body>
 <?php require_once __DIR__ . '/../src/templates/partials/headers.inc.php';
 ?>
-
-<div>
-    <h1 class="espace_banque">MON ESPACE</h1>
-    <h2> Votre compte a été banni <br> CHEH !</h2>
-    <div class="solde"><a href="./deconnexion.php"><button class="bouton">DECONNEXION</button></a></div>
-
-
-</div>
-</div>
 <?php require_once __DIR__ . '/../src/templates/partials/bouton_scroll_haut.php'; ?>
 
 <?php require_once __DIR__ . '/../src/templates/partials/footer.inc.php'; ?>
