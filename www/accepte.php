6a3fb79edb5e190a34c0a18bb2080dfe7c22e80c
Turbotard <turbotard9@gmail.com>
2023-01-19 20:11:04 +0100

    marche po

--- www/accepte.php
+++ www/accepte.php
@@ -16,14 +16,25 @@ $var3 = $db->prepare('SELECT * FROM users WHERE grade = 1');
 $var3->execute();
 $donnees3 = $var3->fetchAll();
 
-foreach($donnees3 as $donne){
-    if (isset($_POST(class))){
+function recup_click(){
+    $nom_bouton = null;
+    foreach($_POST as $key => $value){
+        if(strpos($key, 'bouton_accepte')===0){
+            $nom_bouton = $key;
+            break;
+        }
+    }
+    return $nom_bouton;
+}
+if(isset($_POST['submit'])){
+    $nom_bouton = recup_click();
+    if($nom_bouton){
+        $id_user = $nom_bouton;
         $var = $db->prepare('UPDATE users SET grade = 10 WHERE id = ?');
-        $var->execute([$donne['id']]);
+        $var->execute([$id_user]);
         header('Location: accepte.php');
     }
 }
-
 ?>
 <?php require_once __DIR__ . '/../src/templates/partials/headers.inc.php';
 ?>
@@ -61,7 +72,7 @@ foreach($donnees3 as $donne){
                                 echo "₫";
                             }
                             echo " ";
-                            echo '<input type="submit" class="bouton_accepte" name='.$donnee['id'].' value="✔️">';
+                            echo '<input type="submit" class="bouton_accepte_depo" name='.$donnee['id'].' value="✔️">';
                             
                             echo "</br>";
                         }
@@ -102,7 +113,7 @@ foreach($donnees3 as $donne){
                                 echo "₫";
                             }
                             echo " ";
-                            echo '<input type="submit" class="bouton_accepte" name='.$donnee['id'].' value="✔️">';
+                            echo '<input type="submit" class="bouton_accepte_retrait" name='.$donnee['id'].' value="✔️">';
                             
                             echo "</br>";
                         }
@@ -127,7 +138,7 @@ foreach($donnees3 as $donne){
                             echo " ";
                             echo $donnee['tel'];
                             echo " ";
-                            echo '<input type="submit" class="bouton_accepte" name='.$donnee['id'].' value="✔️">';
+                            echo '<input type="submit" class="bouton_accepte" name='.$donnee['id'];.' value="✔️">';
                             echo "</br>";
 
                             
