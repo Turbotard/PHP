6e014e4a0140e3350fe42cb6fdac4a55433f3e8f
Turbotard <turbotard9@gmail.com>
2023-01-19 17:23:36 +0100

    test depot qui marche pas

--- www/accepte.php
+++ www/accepte.php
@@ -4,11 +4,25 @@ require_once __DIR__ . '/../src/config.php';
 $page_title = 'Acceptations';
 require_once __DIR__ . '/../src/templates/partials/html_head.php';
 
+$var = $db->prepare('SELECT * FROM deposits WHERE done = 1');
+$var->execute();
+$donnees = $var->fetchAll();
 
 ?>
-<body>
 <?php require_once __DIR__ . '/../src/templates/partials/headers.inc.php';
 ?>
+<body>
+    <div class="infos">
+        <div class="ligne2">
+            <h2>Demande Dépôts</h2>
+            <?php
+                foreach($donnes as $donnee){
+                    echo $donnee['somme'];
+                }
+            ?>
+        </div>
+    </div>
+
 <?php require_once __DIR__ . '/../src/templates/partials/bouton_scroll_haut.php'; ?>
 
 <?php require_once __DIR__ . '/../src/templates/partials/footer.inc.php'; ?>
