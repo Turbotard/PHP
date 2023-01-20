ecb954b73287da4dc97b5ca91be29090ba69f580
nom2 <113614747+Stanewolfer@users.noreply.github.com>
2023-01-20 10:55:29 +0100

    samba ban

--- www/ban.php
+++ www/ban.php
@@ -12,7 +12,11 @@ require_once __DIR__ . '/../src/templates/partials/html_head.php';
 
 <div>
     <h1 class="espace_banque">MON ESPACE</h1>
-    <h2> Votre compte a été banni <br> CHEH !</h2>
+    <h2> Votre compte a été banni <br> CHEH ! <br> <br>
+    <audio controls autoplay>
+        <source src="/assets/banni.mp3" type="audio/mpeg">
+    </audio>
+</h2>
     <div class="solde"><a href="./deconnexion.php"><button class="bouton">DECONNEXION</button></a></div>
 
 
